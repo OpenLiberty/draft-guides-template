76efa61ca815e91f36604c139f574239ef8e801d
Nimrah Gill <Nimrah.Gill@ibm.com>
2019-11-26 15:43:26 -0500

    added test execution order

--- finish/src/test/java/it/io/openliberty/guides/todolistSample/TodoIT.java
+++ finish/src/test/java/it/io/openliberty/guides/todolistSample/TodoIT.java
@@ -19,15 +19,31 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class TodoIT {
+
     @Test
+    @Order(1)
     public void testGetTodos() throws Exception {
         HttpClient client = HttpClients.createDefault();
         HttpGet request = new HttpGet("http://localhost:9080/todo-app/todo");
         HttpResponse response = client.execute(request);
 
-        assertEquals(response.getStatusLine().getStatusCode(), 200);
+        assertEquals(200, response.getStatusLine().getStatusCode(), "Incorrect response code from url");
+    }
+
+    @Test
+    @Order(2)
+    public void testGetFirstTodo() throws Exception {
+        HttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet("http://localhost:9080/todo-app/todo" + "/4");
+        HttpResponse response = client.execute(request);
+
+        assertEquals(200, response.getStatusLine().getStatusCode(), "Incorrect response code from url");
     }
 }
